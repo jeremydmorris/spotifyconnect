4d2653f299f12f62e6a68ade1752534952fa327b
seedatarun <jeremydmorris@gmail.com>
2017-05-17 06:34:37 -0600

    Add get_registration_url

---
+++ R/get_registration_url.R
@@ -0,0 +1,17 @@
+#' Combine id/secret and return header.
+#'
+#' Combine id/secret and return header.
+#'
+#' @param scope List describing the kind of access being requested.
+#' @return URL that will request access to a user's Spotify data.
+#' @export
+#' @examples
+#' get_registration_url()
+get_registration_url <- function(scope=c('playlist-read-collaborative','user-library-read','user-follow-read','user-read-email','user-read-private','user-top-read')){
+	base_url <- 'https://accounts.spotify.com/authorize'
+	out <- paste0(base_url,'?client_id=',Sys.getenv('SPOTIFYCLIENTID'),
+		'&response_type=code',
+		'&redirect_uri=',URLencode(Sys.getenv('SPOTIFYREDIRECTURI'),reserved=TRUE),
+		'&scope=',URLencode(paste(scope,collapse=' ')))
+	return(out)
+}
