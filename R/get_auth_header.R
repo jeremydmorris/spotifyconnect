a214742d3fbeaa1e82ae571789542a070b499aa4
seedatarun <jeremydmorris@gmail.com>
2017-05-29 07:09:56 -0600

    Upgrade to return client credentials if requested.

--- R/get_auth_header.R
+++ R/get_auth_header.R
@@ -3,6 +3,7 @@
 #' Combine id/secret and return header.
 #'
 #' @param tkn use saved token info to get header.
+#' @param type either startup, app or user depending on the situation.
 #' @return object add_headers from httr with correct information.
 #' @examples
 #' get_auth_header()
@@ -15,7 +16,22 @@ get_auth_header <- function(tkn,type='user'){
 	}
 
 	if( type == 'app' ){
-		auth_header <- httr::add_headers('Authorization'=paste('Basic',RCurl::base64(paste(Sys.getenv('SPOTIFYCLIENTID'),Sys.getenv('SPOTIFYCLIENTSECRET'),sep=':'))[[1]]))
+		if( is.null(.spotifyconnect_env$my_app) ){
+			my_headers <- httr::add_headers(c(Authorization=paste('Basic',get_client_info(type='auth64'),sep=' ')))
+			my_body <- list(grant_type='client_credentials')
+			my_token <- httr::content(httr::POST('https://accounts.spotify.com/api/token',my_headers,body=my_body,encode='form'))
+
+			.spotifyconnect_env$my_app <- my_token
+			.spotifyconnect_env$my_app$access_token_created <- Sys.time()
+	
+			auth_header <- httr::add_headers('Authorization'=paste('Bearer',.spotifyconnect_env$my_app$access_token,sep=' '))
+		}else{
+			if( difftime(Sys.time(),.spotifyconnect_env$my_app$access_token_created,units='secs') > 3500 ){
+				.spotifyconnect_env$my_app <- NULL
+				get_auth_header(type='app')
+			}
+			auth_header <- httr::add_headers('Authorization'=paste('Bearer',.spotifyconnect_env$my_app$access_token,sep=' '))
+		}
 	}
 
 	if( type == 'user' ){
